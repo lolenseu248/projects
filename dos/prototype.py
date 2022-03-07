309ebc1ea1aa8d0f28aedf5123926ebcd5aa741a
lolenseu <lolenseu.koleutejeu248@gmail.com>
2022-03-08 01:28:17 +0800

    test

---
+++ dos/prototype.py
@@ -0,0 +1,23 @@
+import os
+import socket
+import time
+from ping3 import ping
+
+soc = socket.socket()
+target = input("enter the ip: ")
+
+def gethost():
+    host = soc.connect(targe)
+    print(host)
+
+    print("ok") # DEBUG:
+
+
+gethost()
+def main():
+    response = os.system("ping -c 1 " + host)
+
+    print("ok") # DEBUG:
+
+
+main()
