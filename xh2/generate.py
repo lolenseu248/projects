e1aacfb6bdf9270d4b4d54992d50858c0a8a13c8
Lolenseu <98665691+lolenseu@users.noreply.github.com>
2022-03-25 18:31:33 +0800

    Add files via upload

--- xh2/generate.py
+++ xh2/generate.py
@@ -5,8 +5,10 @@ import random
 import urllib.request
 
 
-#version
-version = json.loads(open('../src/version.json', "r").read())
+#Files needed to run the program.
+#version = json.loads(open('../src/version.json', "r").read())
+rxh1 = json.loads(open('./src/xh1/rxh1.json', "r").read())
+xh1 = json.loads(open('./src/xh1/xh1.json', "r").read())
 
 
 #source "https://www.ascii-code.com/"
@@ -14,44 +16,62 @@ version = json.loads(open('../src/version.json', "r").read())
 numbers = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
 letterslower = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
 lettersupper = ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
-specialchar = (' ', '~', ':', "'", '+', '[', '\\', '@', '^', '{', '%', '(', '-', '"', '*', '|', ',', '&', '<', '`', '}', '.', '_', '=', ']', '!', '>', ';', '?', '#', '$', ')', '/')
+specialchar = (' ', '~', ':', "'", '+', '[', '@', '^', '{', '%', '(', '-', '\\"', '*', '|', ',', '&', '<', '`', '}', '.', '_', '=', ']', '!', '>', ';', '?', '#', '$', ')', '/', '\\' + '\\')
 controlchar = ('NUL', 'SOH', 'STX', 'ETX', 'EOT', 'ENQ', 'ACK', 'BEL', 'BS', 'HT', 'LF', 'VT', 'FF', 'CR', 'SO', 'SI', 'DLE', 'DC1', 'DC2', 'DC3', 'DC4', 'NAK', 'SYN', 'ETB', 'CAN', 'EM', 'SUB', 'ESC', 'FS', 'GS', 'RS', 'US')
-extendchar = ('Ç', 'ü', 'é', 'â', 'ä', 'à', 'å', 'ç', 'ê', 'ë', 'è', 'ï', 'î', 'ì', 'Ä', 'Å', 'É', '§', 'Æ', 'ô', 'ö', 'ò', 'û', 'ù', 'ÿ', 'Ö', 'Ü', '¢', '£', '¥', '₧', 'ƒ', 'á', 'í', 'ó', 'ú', 'ñ', 'Ñ', 'ª', 'º', '¿', '⌐', '¬', '½', '¼', '¡', '«', '»', '░', '▒', '▓', '│', '┤', '╡', '╢', '╖', '╕', '╣', '║', '╗', '╝', '╜', '╛', '┐', '└', '┴', '├', '├', '─', '┼', '╞', '╟', '╚', '╔', '╩', '╦', '╠', '═', '╬', '╧', '╨', '╤', '╥', '╙', '╘', '╒', '╓', '╫', '╪', '┘', '┌', '█', '▄', '▌', '▐', '▀', 'α', 'ß', 'Γ', 'π', 'Σ', 'σ', 'µ', 'τ', 'Φ', 'Θ', 'Ω','δ', '∞', 'φ', 'ε', '∩', '≡', '±', '≥', '≤', '⌠', '⌡', '÷', '≈', '°', '∙', '·', '√', 'ⁿ', '²', '■')
-additional = ('\\n', '\\r', '\\t', '\\0', '\\xddd')
+extendchar = ('Ç', 'ü', 'é', 'â', 'ä', 'à', 'å', 'ç', 'ê', 'ë', 'è', 'ï', 'î', 'ì', 'Ä', 'Å', 'É', '§', 'Æ', 'ô', 'ö', 'ò', 'û', 'ù', 'ÿ', 'Ö', 'Ü', '¢', '£', '¥', '₧', 'ƒ', 'á', 'í', 'ó', 'ú', 'ñ', 'Ñ', 'ª', 'º', '¿', '⌐', '¬', '½', '¼', '¡', '«', '»', '░', '▒', '▓', '│', '┤', '╡', '╢', '╖', '╕', '╣', '║', '╗', '╝', '╜', '╛', '┐', '└', '┴', '├', '─', '┼', '╞', '╟', '╚', '╔', '╩', '╦', '╠', '═', '╬', '╧', '╨', '╤', '╥', '╙', '╘', '╒', '╓', '╫', '╪', '┘', '┌', '█', '▄', '▌', '▐', '▀', 'α', 'ß', 'Γ', 'π', 'Σ', 'σ', 'µ', 'τ', 'Φ', 'Θ', 'Ω','δ', '∞', 'φ', 'ε', '∩', '≡', '±', '≥', '≤', '⌠', '⌡', '÷', '≈', '°', '∙', '·', '√', 'ⁿ', '²', '■')
+additional = ('\\n', '\\r', '\\t', '\\' + '\\a', '\\' + '\\0')
 addiextrchar = ('©', '®',)
 
 
 #steaching
-printables = numbers + letterslower + lettersupper + specialchar
-dictionary = controlchar + printables + extendchar + additional + addiextrchar
+printables = letterslower + lettersupper + numbers + specialchar
+dictionary = controlchar + extendchar + printables + additional + addiextrchar
+#print(len(dictionary)) ##debuging for total characters 
 
 
 #generating key form dictionary
 def genkey():
     global genkey
-    key = open('key', "w")
+    key = open('key.json', "w")
     key.write('{\n')
+
+
+    #generating json format key
     x = 0
     for i in range(len(dictionary)):
         rangen = ''.join((random.choice('qwertyuiopasdfghjklzcvbnmQWERTYUIOPASDFGHJKLZCVBNM1234567890') for i in range(4)))
         gendata = dictionary[x]
-        firbase = '\"' + gendata +'\"' + ': ' + '\"' + rangen + '\",\n'
-        secbase = '\"' + rangen +'\"' + ': ' + '\"' + gendata + '\",\n'
+        firbase = '\"' + gendata + '\"' + ':' + '\"' + rangen + '\",\n'
+        secbase = '\"' + rangen + '\"' + ':' + '\"' + gendata + '\",\n'
         key.write(firbase)
         key.write(secbase)
-        #genkey = ''.join(firbase + secbase)
-        #print(genkey)
         x += 1
+
+
+    key.write('\"' + "xh1" + '\"' + ':' + '\"' + "ok" + '\"\n')
     key.write('}')
 
 
-#reading the generated key
+#reading the generated key and generating xh1 format of the key
 def readkey():
-    key = json.loads(open('key', "r").read())
-    print(key)
-    os.system('rm -rf key')
+    global key
+    key = json.loads(open('key.json', "r").read())
+    data = str(key)
 
+    #generating xh1 format of the key
+    genfilename = ''.join((random.choice('qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890') for i in range(8)))
+    filename = genfilename + '.key'
+    xh1key = open(filename, 'w')
+    xh1key.write('0x')
+
+    #starting the writing the xh1key
+    counter = 0
+    for i in range(len(data)):
+        procces = data[counter]
+        strap = xh1[procces]
+        xh1key.write(strap[2:])
+        counter += 1
+        
 
 genkey()
-#readkey()
-print(len(dictionary))
+readkey()
