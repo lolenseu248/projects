e2ae094f0efb4a7d26baafd87b41283af41637a1
lolenseu <98665691+lolenseu@users.noreply.github.com>
2022-07-26 12:31:59 +0800

    Add files via upload

--- .workloads/digitalenigma/run.py
+++ .workloads/digitalenigma/run.py
@@ -1,18 +1,25 @@
 import time
 import json
 
-readencodefile = open('file.txt', 'r').read().upper()
+#files
+readencodefile = open('file.txt', 'r').read().upper().rstrip('\n')
 plugboardconfig = json.load(open('plugboardconfig.json'))
 print(plugboardconfig)
 print(readencodefile)
 
+
+#settings in plugboard
 counter = 0
 for x in range(len(readencodefile)):
     z = readencodefile[counter]
+    c = plugboardconfig[z]
+    if c == str(0):
+        c = [key for key, value in dict(plugboardconfig).items() if value == c]
     counter += 1
-    print(z)
-    
-    
+    print(c)
+    #print(z)
+
+#rotttors
 counter_a = 0
 counter_b = 0
 counter_c = 0
@@ -34,4 +41,17 @@ while True:
                 counter_a = 1
             print(counter_a, counter_b, counter_c)
             time.sleep(0.201)
-            
+
+"""
+while True:
+    if counter_a >= 26:
+        counter_b += 1
+        counter_a == 1
+
+        if counter_b >= 26:
+            counter_c += 1
+            counter_b == 1
+
+            if counter_c >= 26:
+                counter_c == 1
+"""
