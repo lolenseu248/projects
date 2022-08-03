031466b8356edfd2145114446e15e1564e5353f5
lolenseu <98665691+lolenseu@users.noreply.github.com>
2022-08-04 05:13:22 +0800

    Update and rename guest-the-hidden-number/game.py to guess-the-hidden-number/game.py

--- guess-the-hidden-number/game.py
+++ guess-the-hidden-number/game.py
@@ -3,37 +3,37 @@ import random, time, os
         
 def start():
     os.system('clear')
-    print("Welcome to Guest the Hidden Number Mini Game!")
+    print("Welcome to Guess the Hidden Number Mini Game!")
 
     time.sleep(2)
-    print("Please Enter the Difficulty, Type \"e\" for Easy with 8 guest count, \"m\" for Medium with 7 guest count, \"h\" for Hard with 6 guest count, and \"a\" for Asia with 3 guest count!")
+    print("Please Enter the Difficulty, Type \"e\" for Easy with 8 guess count, \"m\" for Medium with 7 guess count, \"h\" for Hard with 6 guess count, and \"a\" for Asia with 3 guess count!")
     
     a = input("> ").lower()
     if a == str('e'):
         print("Pooling Number is 1 to 20")
         b = random.randint(1, 20)
-        guest_counter = 8
+        guess_counter = 8
     elif a == str('m'):
         print("Pooling Number is 1 to 50")
         b = random.randint(1, 50)
-        guest_counter = 7
+        guess_counter = 7
     elif a == str('h'):
         print("Pooling Number is 1 to 100")
         b = random.randint(1, 100)
-        guest_counter = 6
+        guess_counter = 6
     elif a == str('a'):
         print("Pooling Number is 1 to 1000")
         b = random.randint(1, 1000)
-        guest_counter = 3
+        guess_counter = 3
     elif a == int():
         exit()
     else:
         start()
 
-    guest = False
-    while not guest:
-        guest_counter -= 1
-        if guest_counter == int(-1):
+    guess = False
+    while not guess:
+        guess_counter -= 1
+        if guess_counter == int(-1):
             if a == str('a'):
                 print(f"Ops your Count is out, The Hidden Number is {b}, You Noob")
             else:
@@ -47,27 +47,27 @@ def start():
             else:
                 exit()
 
-        print("Guest the Number...")
+        print("Guess the Number...")
         c = int(input("> "))
         if b == str():
             print("You Enter a Wrong Character your Game is Restarted")
             time.sleep(2)
             start()
         elif b == c:
-            guest = True
+            guess = True
         elif b < c:
-            print(f"You are to High!, Guest Count {guest_counter}")
+            print(f"You are to High!, Guess Count {guess_counter}")
         elif b > c:
-            print(f"You are to Low!, Guest Count {guest_counter}")
+            print(f"You are to Low!, Guess Count {guess_counter}")
         else:
             exit()
 
     if a == str('e'):
-        print(f"Congratulation You Guest the Number, The Hidden Number is {b}, Hey Try the Medium one!")
+        print(f"Congratulation You Guess the Number, The Hidden Number is {b}, Hey Try the Medium one!")
     elif a == str('m'):
-        print(f"Congratulation You Guest the Number, The Hidden Number is {b}, Hey Try the Hard one!")
+        print(f"Congratulation You Guess the Number, The Hidden Number is {b}, Hey Try the Hard one!")
     elif a == str('h'):
-        print(f"Congratulation You Guest the Number, The Hidden Number is {b}, Hey don't Forget to Beat the Asian one!")
+        print(f"Congratulation You Guess the Number, The Hidden Number is {b}, Hey don't Forget to Beat the Asian one!")
     elif a == str('a'):
         print("So you are Asian! What the Hell!")
 
