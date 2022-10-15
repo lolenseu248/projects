8cb3eb6b4950c426d764b59c1195d461437331f5
lolenseu <98665691+lolenseu@users.noreply.github.com>
2022-10-15 19:20:52 +0800

    Update controller.ino

--- blynk-drone-controller/controller.ino
+++ blynk-drone-controller/controller.ino
@@ -9,8 +9,8 @@
 #include <BlynkSimpleEsp8266.h>
 #include <Servo.h>
 
-#define LED1 5
-#define LED2 16
+#define BUZZER 5
+#define LED1 16
 
 Servo servo1;
 Servo servo2;
@@ -32,70 +32,100 @@ void setup() {
   servo4.attach(15);
   servo5.attach(2);
   servo6.attach(0);
+  pinMode(BUZZER, OUTPUT);
   pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
 }
 
 void loop() {
+  if (Blynk.connected()) {
+    Blynk.run();
+  }
   //Auto Landing if Disconnected to the Internet
-  if (Blynk.connected()) Blynk.run();
   else {
     servo1.write(1500);
     servo2.write(1500);
     servo3.write(1500);
     servo4.write(1500);
     servo5.write(1899);
+    tone(BUZZER,5000,250);
     delay(1000);
     Blynk.run();
   }
 }
 
 BLYNK_WRITE(V1) {
-  int servov1 = param.asInt();
-  servo1.write(servov1);
-  //Serial.println(servov1);
+  if (LED1 == 1) {
+    int servov1 = map(param.asInt(),1000,2000,1300,1700);
+    servo1.write(servov1);
+  }
+  else {
+    int servov1 = param.asInt();
+    servo1.write(servov1);
+    if (servov1 == 1000) tone(BUZZER,5000);
+    else noTone(BUZZER);
+  }
 }
 
 BLYNK_WRITE(V2) {
-  int servov2 = map(param.asInt(),1000,2000,2000,1000);
-  servo2.write(servov2);
-  //Serial.println(servov2);
+  if (LED1 == 1) {
+    int servov2 = map(param.asInt(),1000,2000,1700,1300);
+    servo2.write(servov2);
+  }
+  else {
+    int servov2 = map(param.asInt(),1000,2000,2000,1000);
+    servo2.write(servov2);
+  }
 }
 
 BLYNK_WRITE(V3) {
-  int servov3 = map(param.asInt(),1000,2000,2000,1000);
-  servo3.write(servov3);
-  //Serial.println(servov3);
+  if (LED1 == 1) {
+    int servov3 = map(param.asInt(),1000,2000,1700,1300);
+    servo3.write(servov3);
+  }
+  else {
+    int servov3 = map(param.asInt(),1000,2000,2000,1000);
+    servo3.write(servov3);
+  } 
 }
 
 BLYNK_WRITE(V4) {
-  int servov4 = map(param.asInt(),1000,2000,2000,1000);
-  servo4.write(servov4);
-  //Serial.println(servov4);
+  if (LED1 == 1) {
+    int servov4 = map(param.asInt(),1000,2000,1700,1300);
+    servo4.write(servov4);
+  }
+  else {
+     int servov4 = map(param.asInt(),1000,2000,2000,1000);
+   servo4.write(servov4);
+  }
 }
 
 BLYNK_WRITE(V5) {
   int servov5 = map(param.asInt(),1,6,1100,1900);
   servo5.write(servov5);
-  //Serial.println(servov5);
 }
-
+/*
 BLYNK_WRITE(V6) {
   int servov6 = param.asInt();
-  servo6.write(servov6);
- //Serial.println(servov6);
+  if (servov6 == 1) servo6.write(servov1);
+  else servo6.write(2000);
 }
-
+*/
 BLYNK_WRITE(V7) {
-  int ledv1 = param.asInt();
-  if (ledv1 == 1) digitalWrite(LED1, HIGH);
-  else digitalWrite(LED1, LOW);
-  //Serial.prinrln(ledv1);
+  int buzzerv7 = param.asInt();
+  if (buzzerv7 == 1) tone(BUZZER,5000);
+  else noTone(BUZZER);
 }
 
 BLYNK_WRITE(V8) {
-  int ledv2 = param.asInt();
-  if (ledv2 == 1) digitalWrite(LED2, HIGH);
-  else digitalWrite(LED2, LOW);
-  //Serial.println(ledv2);
+  int ledv8 = param.asInt();
+  if (ledv8 == 1) digitalWrite(LED1, HIGH);
+  else digitalWrite(LED1, LOW);
+}
+
+BLYNK_WRITE(V9) {
+  int buttonv9 = param.asInt();
+}
+
+BLYNK_WRITE(V10) {
+  int buttonv10 = param.asInt();
 }
