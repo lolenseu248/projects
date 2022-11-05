cd30cd4f2b043d5a2b796ca98326a7601abe1dac
lolenseu <98665691+lolenseu@users.noreply.github.com>
2022-11-05 17:48:02 +0800

    Update blynk-drone-controller.ino

--- blynk-drone-controller/blynk-drone-controller.ino
+++ blynk-drone-controller/blynk-drone-controller.ino
@@ -6,90 +6,81 @@
 #define BLYNK_PRINT Serial
 
 #include <ESP8266WiFi.h>
+#include <ESP8266Ping.h>
 #include <BlynkSimpleEsp8266.h>
-#include <ArduinoTimer.h>
 #include <Servo.h>
 
 
-ArduinoTimer Timer1;
-ArduinoTimer Timer2;
+char auth[] = BLYNK_AUTH_TOKEN; 
+char ssid[] = WIFI_SSID;                    
+char pass[] = WIFI_PASSWORD;
 
-Servo servo1;
-Servo servo2;
-Servo servo3;
-Servo servo4;
-Servo servo5;
-Servo servo6;
+Servo servo1,servo2,servo3,servo4,servo5,servo6;
 
 int counter = 0;
 
 int BUZZER = 5;
 int LED1 = 16;
 
-char auth[] = BLYNK_AUTH_TOKEN; 
-char ssid[] = WIFI_SSID;                    
-char pass[] = WIFI_PASSWORD;
-
-int rcValue1;
-int rcValue2;
-int rcValue3;
-int rcValue4;
-int rcValue5;
-int rcValue6;
-int rcValue7;
-int rcValue8;
-int rcValue9;
-int rcValue10;
-
 int prcValue1;
-int prcValue2;
-int prcValue3;
-int prcValue4;
-int prcValue5;
 
-int nrcValue1 = 1600;
-int nrcValue2 = 1500;
-int nrcValue3 = 1500;
-int nrcValue4 = 1500;
-int nrcValue5 = 1100;
+int rcValue1 = 1600;
+int rcValue2 = 1500;
+int rcValue3 = 1500;
+int rcValue4 = 1500;
+int rcValue5 = 1100;
+int rcValue6 = 2000;
+int rcValue7 = 0;
+int rcValue8 = 0;
+int rcValue9 = 0;
+int rcValue10 = 0;
 
 
 BLYNK_WRITE(V1) {
-  rcValue1 = param.asInt();
-  prcValue1 = map(rcValue1,1000,2000,1000,2000);
-  if (rcValue9 == 1) nrcValue1 = map(prcValue1,1000,2000,1400,1800);
-  else if (rcValue1 == 1000) tone(BUZZER,5000);
-  else nrcValue1 = prcValue1;
+  if (rcValue9 == 1) {
+      prcValue1 = param.asInt();
+    //prcValue1 = map(param.asInt(),1000,2000,1400,1800);
+  }
+  else rcValue1 = param.asInt();
+  //servo1.write(rcValue1);
 }
 
 BLYNK_WRITE(V2) {
-  rcValue2 = param.asInt();
-  prcValue2 = map(rcValue2,1000,2000,2000,1000);
-  if (rcValue9 == 1) nrcValue2 = map(prcValue2,2000,1000,1700,1300);
-  else nrcValue2 = prcValue2;
+  if (param.asInt()) {
+    if (rcValue9 == 1) rcValue2 = map(param.asInt(),1000,2000,1700,1300);
+    else rcValue2 = map(param.asInt(),1000,2000,2000,1000);
+    //servo2.write(rcValue2);
+  }
+  else rcValue2 = 1500;
 }
 
 BLYNK_WRITE(V3) {
-  rcValue3 = param.asInt();
-  prcValue3 = map(rcValue3,1000,2000,2000,1000);
-  if (rcValue10 == 1) nrcValue3 = map(prcValue3,2000,1000,1700,1300);
-  else nrcValue3 = prcValue3;
+  if (param.asInt()) {
+    if (rcValue10 == 1) rcValue3 = map(param.asInt(),1000,2000,1700,1300);
+    else rcValue3 = map(param.asInt(),1000,2000,2000,1000);
+    //servo3.write(rcValue3);
+  }
+  else rcValue3 = 1500;
 }
 
 BLYNK_WRITE(V4) {
-  rcValue4 = param.asInt();
-  prcValue4 = map(rcValue4,1000,2000,2000,1000);
-  if (rcValue10 == 1) nrcValue4 = map(prcValue4,2000,1000,1700,1300);
-  else nrcValue4 = prcValue4;
+  if (param.asInt()) {
+    if (rcValue9 == 1) rcValue4 = map(param.asInt(),1000,2000,1700,1300);
+    else rcValue4 = map(param.asInt(),1000,2000,2000,1000);
+    //servo4.write(rcValue4);
+  }
+  else rcValue4 = 1500;
 }
 
 BLYNK_WRITE(V5) {
-  rcValue5 = param.asInt();
-  nrcValue5 = map(rcValue5,1,6,1100,1900);
+  rcValue5 = map(param.asInt(),1,6,1100,1900);
+ // servo5.write(rcValue5);
 }
 
 BLYNK_WRITE(V6) {
   rcValue6 = param.asInt();
+  //if (rcValue6 == 1) servo6.write(rcValue1);
+  //else servo6.write(2000);
 }
 
 BLYNK_WRITE(V7) {
@@ -125,12 +116,6 @@ void setup() {
   servo5.attach(2);
   servo6.attach(0);
   
-  servo1.write(nrcValue1);
-  servo2.write(nrcValue2);
-  servo3.write(nrcValue3);
-  servo4.write(nrcValue4);
-  servo5.write(nrcValue5);
-  
   //DJI Tone Start Up
   delay(3000);
   tone(BUZZER,800,550);
@@ -138,7 +123,7 @@ void setup() {
   tone(BUZZER,1200,550);
   delay(200);
   tone(BUZZER,1050,550);
-  delay(1000);
+  delay(1500);
   
   //5sec. Delay.
   delay(5000);
@@ -147,90 +132,65 @@ void setup() {
 
 void loop() {
   if (Blynk.connected()) {
+    
+    //Run the Blynk Fuctions.
     Blynk.run();
 
-    //Buzzer and Led.
-    if (Timer1.TimePassed_Milliseconds(3000)) {
+    //Tuning Throttle.
+    if (rcValue9 == 1) {
+      if (prcValue1 > 1600) rcValue1 += 1;
+      if (prcValue1 < 1400) rcValue1 -= 1;
+      if (rcValue1 >= 1800) prcValue1 = 1600,rcValue1 = 1600;
+      if (rcValue1 <= 1400) prcValue1 = 1600,rcValue1 = 1600;
+    }
+
+    //Sending Signal to ArduPilot.
+    servo1.write(rcValue1);
+    servo2.write(rcValue2);
+    servo3.write(rcValue3);
+    servo4.write(rcValue4);
+    servo5.write(rcValue5);
+
+    //For Manual ESC Calibration
+    if (rcValue6 == 1) servo6.write(rcValue1);
+    else servo6.write(2000);
+    
+    //Led, Buzzer, and Ping.
+    if (counter == 1) {
       digitalWrite(LED1, HIGH);
       tone(BUZZER,3500,250);
     }
-    else if (Timer2.TimePassed_Milliseconds(3500)) {
-      digitalWrite(LED1, LOW);
-      Timer1.Reset();
-      Timer2.Reset();
-    }
 
-    //Saver Functions.
-    //Auto Loiter if Untouch.
-    if (counter == 250) {
-      if (nrcValue5 == 1580) nrcValue5 = 1580, counter = 0;
-      else if (nrcValue5 == 1740) nrcValue5 = 1740, counter = 0;
-      else if (nrcValue5 == 1900) nrcValue5 = 1900, counter = 0;
-      else if (rcValue3 == 1500 and rcValue4 == 1500) nrcValue5 = 1420, counter = 300;
-      else counter = 0;
+    if (counter == 10) {
+      digitalWrite(LED1, LOW);     
     }
     
-    //Auto Landing if Loiter Mode Untouch.
-    else if (counter == 1000) {
-      if (nrcValue5 == 1420) nrcValue5 = 1899, counter = 0;
-      else counter = 0;
+    if (counter == 100) {
+      Ping.ping("sgp1.blynk.cloud", 1); //Edit this to your Blynk Server.
+      Blynk.virtualWrite(V11, Ping.averageTime());
+      Blynk.virtualWrite(V12, map(rcValue5,1100,1900,1,6));
+      counter = 0;
     }
     
     counter += 1;
-    
-    //Sending PWM Signal to ArduPilot.
-    servo1.write(nrcValue1);
-    servo2.write(nrcValue2);
-    servo3.write(nrcValue3);
-    servo4.write(nrcValue4);
-    servo5.write(nrcValue5);
-  
-    if (rcValue6 == 1) servo6.write(nrcValue1);
-    else servo6.write(2000);
-    
-    Blynk.virtualWrite(V12, map(nrcValue5,1100,1900,1,6));
 
     //Debugging.
-    Serial.print("Trottle: ");
-    Serial.print(rcValue1);
-    Serial.print(" / ");
-    Serial.print(nrcValue1);
-    Serial.print("\t");
-    Serial.print("Yaw: ");
-    Serial.print(rcValue2);
-    Serial.print(" / ");
-    Serial.print(nrcValue2);
-    Serial.print("\t");
-    Serial.print("Pitch: ");
-    Serial.print(rcValue3);
-    Serial.print(" / ");
-    Serial.print(nrcValue3);
-    Serial.print("\t");
-    Serial.print("Roll: ");
-    Serial.print(rcValue4);
-    Serial.print(" / ");
-    Serial.print(nrcValue4);
-    Serial.print("\t");
-    Serial.print("Mode: ");
-    Serial.print(rcValue5);
-    Serial.print(" / ");
-    Serial.print(nrcValue5);
-    Serial.print("\t");
-    Serial.print("Counter: ");
-    Serial.println(counter);
+    Serial.printf("Trottle: %d  |  Yaw: %d  |  Pitch: %d  |  Roll: %d  |  Mode: %d  |  Counter: %d \n",rcValue1,rcValue2,rcValue3,rcValue4,rcValue5,counter);
     //delay(100); //Enable this for Debugging.
   }
+   
   //Auto Landing if Disconnected to the Internet.
   else {
     servo1.write(1600);
     servo2.write(1500);
     servo3.write(1500);
     servo4.write(1500);
-    servo5.write(1899);
-    digitalWrite(LED1, LOW);
-    tone(BUZZER,5000,250);
-    delay(2500);
+    servo5.write(1899); //ArduPilot Mode No.6 "Land".
     digitalWrite(LED1, HIGH);
+    tone(BUZZER,5000,250);
+    delay(500);
+    digitalWrite(LED1, LOW);
+    delay(1000);
     Blynk.run();
   }
 }
