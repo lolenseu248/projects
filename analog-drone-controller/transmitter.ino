b7f11f00c926bd7ca417c8005cb72646a16a00e9
lolenseu <98665691+lolenseu@users.noreply.github.com>
2022-11-24 00:13:23 +0800

    Update transmitter.ino

--- analog-drone-controller/transmitter.ino
+++ analog-drone-controller/transmitter.ino
@@ -1,25 +1,35 @@
 #include <SPI.h>
-#include <nRF24L01.h>
+#include <Wire.h>
 #include <RF24.h>
+#include <nRF24L01.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
 
 //Pinouts
-RF24 radio(1, 3);
+RF24 radio(1,3);
 
 //Joystic Pinouts
-int joyX1 = 15;
-int joyY1 = 2;
-int joySW1 = 17;
+int joyX1=15;
+int joyY1=2;
+int joySW1=17;
 
-int joyX2 = 0;
-int joyY2 = 4;
-int joySW2 = 16;
+int joyX2=0;
+int joyY2=4;
+int joySW2=16;
 
 //Potentiometer Pinouts
-int potenMeter1 = 12;
-int potenMeter2 = 14;
+int potenMeter1=12;
+int potenMeter2=14;
+
+//Toggle Switchs
+int togSW1=35;
+int togSW2=34;
+int togSW3=39;
+int togSW4=36;
 
 
+//Variables
 //Joystic Inputs and Maps
 int joyX1Pos;
 int joyY1Pos;
@@ -43,7 +53,7 @@ int potenM2Pos;
 
 char msg[32];
 
-const byte address[6] = "00001";
+const byte address[6]="00001";
 
 
 void setup() {
@@ -53,39 +63,45 @@ void setup() {
   radio.setDataRate(RF24_250KBPS);
   radio.stopListening();
 
-  pinMode(joyX1, INPUT);
-  pinMode(joyY1, INPUT);
-  pinMode(joySW1, INPUT_PULLUP);
-
-  pinMode(joyX2, INPUT);
-  pinMode(joyY2, INPUT);
-  pinMode(joySW2, INPUT_PULLUP);
-
-  pinMode(potenMeter1, INPUT);
-  pinMode(potenMeter2, INPUT);  
+  pinMode(joyX1,INPUT);
+  pinMode(joyY1,INPUT);
+  pinMode(joySW1,INPUT_PULLUP);
   
+  pinMode(joyX2,INPUT);
+  pinMode(joyY2,INPUT);
+  pinMode(joySW2,INPUT_PULLUP);
+
+  pinMode(potenMeter1,INPUT);
+  pinMode(potenMeter2,INPUT);
+
+  pinMode(togSW1,INPUT);
+  pinMode(togSW2,INPUT);
+  pinMode(togSW3,INPUT);
+  pinMode(togSW4,INPUT);
 }
 
 
 void loop() {
-  joyX1Pos = analogRead(joyX1);
-  joyY1Pos = analogRead(joyY1);
-  joySW1State = digitalRead(joySW1);
+  //Proccessing the inputs data
+  joyX1Pos=analogRead(joyX1);
+  joyY1Pos=analogRead(joyY1);
+  joySW1State=digitalRead(joySW1);
 
-  joy_mapX1 = map(joyX1Pos, 0, 1023, 1000, 2000);
-  joy_mapY1 = map(joyY1Pos, 0, 1023, 1000, 2000);
+  joy_mapX1=map(joyX1Pos,0,1023,1000,2000);
+  joy_mapY1=map(joyY1Pos,0,1023,1000,2000);
 
-  joyX2Pos = analogRead(joyX2);
-  joyY2Pos = analogRead(joyY2);
-  joySW2State = digitalRead(joySW2);
+  joyX2Pos=analogRead(joyX2);
+  joyY2Pos=analogRead(joyY2);
+  joySW2State=digitalRead(joySW2);
 
-  joy_mapX2 = map(joyX2Pos, 0, 1023, 1000, 2000);
-  joy_mapY2 = map(joyY2Pos, 0, 1023, 1000, 2000);
+  joy_mapX2=map(joyX2Pos,0,1023,1000,2000);
+  joy_mapY2=map(joyY2Pos,0,1023,1000,2000);
 
-  potenM1Pos = analogRead(potenMeter1);
-  potenM2Pos = analogRead(potenMeter1);
+  potenM1Pos=analogRead(potenMeter1);
+  potenM2Pos=analogRead(potenMeter1);
 
 
-  
-  radio.write(&msg, sizeof(msg));
+
+  //Sending to rNF24L01 data
+  radio.write(&msg,sizeof(msg));
 }
