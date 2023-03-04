f0af50eb51b8d03ed74e48689934caf76c912483
lolenseu <98665691+lolenseu@users.noreply.github.com>
2023-03-04 21:43:42 +0800

    Update transmitter.ino

--- analog-drone-controller/transmitter.ino
+++ analog-drone-controller/transmitter.ino
@@ -3,6 +3,11 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino resetpin)
+
 //Joystic Pinouts
 int joyX1=19;
 int joyY1=18;
@@ -47,9 +52,15 @@ int potenM2Pos;
 
 char msg[32];
 
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
 
 void setup() {
   Serial.begin(115200);
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;); // Don't proceed, loop forever
+  }
   
   pinMode(joyX1,INPUT);
   pinMode(joyY1,INPUT);
@@ -87,6 +98,13 @@ void loop() {
 
   potenM1Pos=analogRead(potenMeter1);
   potenM2Pos=analogRead(potenMeter1);
-
+  
+  
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 28);
+  display.println("Hello world!");
+  display.display();
+  display.clearDisplay();
 
 }
